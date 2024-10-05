ee8c9cf980eb1681656a655d7a6741a043be76b3
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-10-05 18:13:52 +0900

    first commit

---
+++ src/prac01.ts
@@ -0,0 +1,24 @@
+export {};
+//const userName: string = "Bob";
+//console.log(`Hey, ${userName}.`);
+
+const name: string = "TypeScriotの勉強";
+const priority: number = 3;
+console.log('Todo 1 => ${name} (優先度:${priority})');
+
+/*
+// Date型の変数 deadline の宣言と初期化
+let deadline: Date = new Date(2024, 10, 2, 11, 45);
+
+
+// 年、月、日、時間、分を取得
+let year = deadline.getFullYear();
+let month = (deadline.getMonth() + 1).toString().padStart(2, '0'); // 月は0から始まるので+1
+let day = deadline.getDate().toString().padStart(2, '0');
+let hours = deadline.getHours().toString().padStart(2, '0');
+let minutes = deadline.getMinutes().toString().padStart(2, '0');
+
+// フォーマットされた文字列を作成
+let formattedDate = `${year}/${month}/${day} ${hours}:${minutes}`;
+console.log(formattedDate); // "2024/11/02 11:45" の形式で出力される
+*/
