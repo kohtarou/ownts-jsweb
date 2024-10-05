3bb9d171ea0ad7e4f9fdcbad681bef20a0d0cf0d
koh8901 <120536905+kohtarou@users.noreply.github.com>
2024-10-05 20:21:58 +0900

    import react

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,18 @@
+/*
 "use strict";
 //console.log("■ prac00.ts の実行");
 //import "./prac00";
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("■ prac01.ts の実行");
 require("./prac01");
+*/
+
+import { createRoot } from 'react-dom/client';
+
+// Clear the existing HTML content
+document.body.innerHTML = '<div id="app"></div>';
+
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<h1>Hello, world</h1>);
+
